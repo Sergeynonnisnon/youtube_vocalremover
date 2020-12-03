0e987ecbdf16cc9c5fe32f687f96eca5e26110d2
Serg <goszzakupki@yandex.ru>
2020-12-03 23:09:19 +0300

    firefox driver work , but dont download

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
 
 import pytube
 from selenium import webdriver
+import os,time
 
 URL='https://www.youtube.com/watch?v=dQw4w9WgXcQ'
 filename ='nevergiveup'
 
 #thise script download audio from youtube.com and processing on  https://vocalremover.org
 
+driver = webdriver.Firefox()
+driver.get('https://vocalremover.org')
+upl_btn = driver.find_element_by_id(id_='input-audio-file').send_keys(os.getcwd() + "\\nevergiveup.webm")
+
+def check_exists_by_xpath(xpath):
+    try:
+        driver.find_element_by_xpath(xpath)
+    except :
+        return False
+    return True
+for i in range(150):
+    if check_exists_by_xpath('//*[@id="app"]/main/div[2]/button[2]') == True:
+        driver.find_element_by_xpath('//*[@id="app"]/main/div[2]/button[2]').click()
+        print('найдено')
+        break
+    else:
+        print('не найдено')
+        time.sleep(1)
+      #player-codec second
+#print(upl_btn)
 def get_audio (URL,filename):
     yt = pytube.YouTube(URL)
     streams = yt.streams
@@ -14,14 +35,13 @@ def get_audio (URL,filename):
     print ('Готово')
     return audio_best.download(filename=filename)
 
-def main():
-    driver = webdriver.Firefox('/geckodriver.exe')
-    print(driver.get("https://vocalremover.org/en/"))
-    upl_btn = driver.find_element_by_class_name(name='upload')
-    print (upl_btn)
 
-if __name__=='main':
-    main()
+
+#def main():
+
+
+#if __name__=='main':
+#    main()
 
 
 
