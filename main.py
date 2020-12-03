21123d12d702c355cbe2855f8bdf7ea510021acd
Serg <goszzakupki@yandex.ru>
2020-12-03 21:08:50 +0300

    selenium started

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 
 import pytube
+from selenium import webdriver
 
 URL='https://www.youtube.com/watch?v=dQw4w9WgXcQ'
 filename ='nevergiveup'
+
 #thise script download audio from youtube.com and processing on  https://vocalremover.org
+
 def get_audio (URL,filename):
     yt = pytube.YouTube(URL)
     streams = yt.streams
     audio_best = streams.filter(only_audio=True).desc().first()
     print ('Готово')
     return audio_best.download(filename=filename)
+
+def main():
+    driver = webdriver.Chrome()
+    driver.get("https://vocalremover.org/en/")
+
+if __name__=='main':
+    main()
 
 
 
